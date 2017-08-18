b517aa89eb2da8cbe20c5a469b31bb7ab66a4d86
Tim Haak <tim@haak.co>
2017-08-18 15:20:02 +0200

    Updated example run with mounts

--- runExample.sh
+++ runExample.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+docker stop tm
+docker rm -f tm
 docker run \
   -d \
+  --restart=always \
+  --hostname tm.haak.co \
   --name tm \
+  -v /docker/tm/logs:/site/logs \
+  -v /docker/tm/db:/site/db \
   -p 80:80 \
   timhaak/tech-maturity
